e29b4cafb10cb8f20eaf2f93e0a5f25ca2719809
kra5alllez <kra5alllez@gmail.com>
2021-03-29 00:27:05 +0300

    add project

---
+++ Module_3_Task_5_Vasylchenko/Module_3_Task_5_Vasylchenko/HomeWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3_Task_5_Vasylchenko
+{
+    public class HomeWork
+    {
+        public async Task<string> WorldAsync()
+        {
+           return await Task.Run(() => "World");
+        }
+
+        public async Task<string> HelloAsync()
+        {
+            return await Task.Run(() => File.ReadAllTextAsync("TextFile1.txt"));
+        }
+
+        public async Task<string> ConcontenAsync()
+        {
+            var k = await Task.WhenAll(HelloAsync(), WorldAsync());
+            foreach (var y in k)
+            {
+                Console.WriteLine(y);
+            }
+
+            return await Task.Run(() => " ");
+        }
+
+        public void RR()
+        {
+            Console.WriteLine(ConcontenAsync().GetAwaiter().GetResult());
+        }
+    }
+}
---
+++ Module_3_Task_5_Vasylchenko/Module_3_Task_5_Vasylchenko/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module_3_Task_5_Vasylchenko
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HomeWork w = new HomeWork();
+            w.RR();
+        }
+    }
+}
