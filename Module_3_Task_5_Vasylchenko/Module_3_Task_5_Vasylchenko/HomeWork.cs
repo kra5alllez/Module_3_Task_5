f1b78569aac92f5621fcc539208ddce9a4f3b7f7
kra5alllez <kra5alllez@gmail.com>
2021-03-30 20:53:09 +0300

    changes

--- Module_3_Task_5_Vasylchenko/Module_3_Task_5_Vasylchenko/HomeWork.cs
+++ Module_3_Task_5_Vasylchenko/Module_3_Task_5_Vasylchenko/HomeWork.cs
@@ -7,22 +7,20 @@ namespace Module_3_Task_5_Vasylchenko
 {
     public class HomeWork
     {
-        private readonly string _world;
-        private readonly string _path;
+        private string _path;
 
         public HomeWork()
         {
-            _world = "World";
             _path = "TextFile1.txt";
         }
 
-        public Task<string> HomeWorkAsync()
+        public async Task<string> HomeWorkAsync()
         {
-            List<Task<string>> list = new List<Task<string>>();
-            list.Add(Task.FromResult(_world));
+            var list = new List<Task<string>>();
+            list.Add(Task.FromResult("World"));
             list.Add(Task.Run(() => File.ReadAllTextAsync(_path)));
-            var k = Task.WhenAll(list);
-            return Task.Run(() => $"{k.Result[1]} {k.Result[0]}");
+            var k = await Task.WhenAll(list);
+            return $"{k[1]} {k[0]}";
         }
     }
 }
